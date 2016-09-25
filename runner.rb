3232382e37e24ae9f9c04a5f483f1b729377717e
PeterWuMC <peter.wu@simplybusiness.co.uk>
2016-09-25 10:09:57 +0100

    simple runner

--- runner.rb
+++ runner.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
 require './app/bitmap_editor'
+require './app/command'
 
-BitmapEditor.new.run
+@running       = true
+@bitmap_editor = BitmapEditor.new
+
+puts 'type ? for help'
+while true
+  print '> '
+  result = Command.new(gets.chomp, @bitmap_editor).execute
+  puts result == false ? 'command faild' : 'command successful'
+end
